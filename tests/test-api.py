72ec2917e28b703baf8eb5d94bcedde5f0aa3fa7
doublebyte1 <info@doublebyte.net>
2024-12-19 17:57:57 +0000

    - fixed tests code

--- tests/test-api.py
+++ tests/test-api.py
@@ -1,21 +1,19 @@
 import pytest
 import requests
 
-# Define the URL to test
-URL = 'http://localhost:5000'
-
+base_url = 'http://localhost:5000'
 collection = 'http://localhost:5000/collections/hyderabad/items'
 
-# Test function to check if the server is responding
+# Check if the server is responding
 def test_server_responds():
     try:
-        response = requests.get(URL)
+        response = requests.get(base_url)
         # Check if the status code is 200 (OK)
         assert response.status_code == 200
     except requests.exceptions.RequestException as e:
         pytest.fail(f'Server not responding: {e}')
 
-# Test function to check if the hyderabad collection is published
+# Check if the hyderabad collection is published
 def test_collection_published():
     try:
         response = requests.get(collection)
